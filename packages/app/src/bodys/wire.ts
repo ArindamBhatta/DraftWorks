6acd4465473869af633a268981121bf746afa38c
Arindam Bhattacharyya <arindambhattacharyya.ab@gmail.com>
2026-08-24 08:47:56 +0530

    feat(app): implement 2D parametric body nodes for points, lines, arcs, circles, and polygons

---
+++ packages/app/src/bodys/wire.ts
@@ -0,0 +1,44 @@
+// Part of the Chili3d Project, under the AGPL-3.0 License.
+// See LICENSE file in the project root for full license information.
+
+import {
+    type I18nKeys,
+    type IDocument,
+    type IEdge,
+    type IShape,
+    ParameterShapeNode,
+    type Result,
+    serializable,
+    serialize,
+} from "@chili3d/core";
+
+export interface WireOptions {
+    document: IDocument;
+    edges: IEdge[];
+}
+
+// 2D-compatible - joins edges into a wire (curve chain), compatible with a 2D
+// drafting build.
+@serializable()
+export class WireNode extends ParameterShapeNode {
+    override display(): I18nKeys {
+        return "body.wire";
+    }
+
+    @serialize()
+    get edges(): IEdge[] {
+        return this.getPrivateValue("edges");
+    }
+    set edges(values: IEdge[]) {
+        this.setPropertyEmitShapeChanged("edges", values);
+    }
+
+    constructor(options: WireOptions) {
+        super(options);
+        this.setPrivateValue("edges", options.edges);
+    }
+
+    override generateShape(): Result<IShape> {
+        return shapeFactory.wire(this.edges);
+    }
+}
